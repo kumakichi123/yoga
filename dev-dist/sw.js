3cc318b44f15b571004aa891b995f98913bea38e
kumakichi123 <asabe7729@gmail.com>
2025-10-04 22:25:51 +0900

    a#

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hi8bd1ok44"
+    "revision": "0.7l75vtkfb0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
